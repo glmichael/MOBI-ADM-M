5abf7f41371a9538516962c93d687436de1a92ee
Chrimi8 <chrimi8@gmx.de>
2017-06-01 22:11:08 +0200

    main method for testing

--- TL1/FPStream/src/main/Main.java
+++ TL1/FPStream/src/main/Main.java
@@ -1,9 +1,33 @@
 package main;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import scan.ScanManager;
+import tree.TreeManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int windowsize = 100;
+		int xi = 3;
+
+		ScanManager scanManager = new ScanManager(windowsize, new File("market_data.csv"), xi);
+		ArrayList<ArrayList<String>> transactions;
+
+		int counter = 0;
+		while ((transactions = scanManager.scanNextWindow()) != null) {
+			TreeManager treeManager = new TreeManager();
+			for (ArrayList<String> transaction : transactions) {
+				if (!transaction.isEmpty()) {
+					treeManager.addTransaction(transaction);
+				}
+			}
+			treeManager.printTree();
+
+			System.out.println(counter);
+			counter += windowsize;
+		}
 
 	}
 
